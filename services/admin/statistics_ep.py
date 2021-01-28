3e151e93937fbe2c1e8bb1e5bd4c5f06c7906074
awtoh <ashton0606@gmail.com>
2021-01-28 10:25:38 +0800

    turned more data from the dashboard.html to data in statistic_ep.py

--- services/admin/statistics_ep.py
+++ services/admin/statistics_ep.py
@@ -23,10 +23,10 @@ def page_dashboard():
     quarter2 = ["April", "May", "June"]
     quarter3 = ["July", "August", "September"]
     quarter4 = ["October", "November", "December"]
-    quarter1expenses = ["January", "Feburary", "March"]
-    quarter2expenses = ["April", "May", "June"]
-    quarter3expenses = ["July", "August", "September"]
-    quarter4expenses = ["October", "November", "December"]
+    quarter1expenses = [54, 120, 74]
+    quarter2expenses = [69, 1337, 420]
+    quarter3expenses = [93, 21, 37]
+    quarter4expenses = [320, 203, 416]
     if request.method == "POST":
         print("hello world")
-    return render_template("admin/dashboard.html", datasets=datasets, labels=labels, days=days, quarter1=quarter1, quarter2=quarter2, quarter3=quarter3, quarter4=quarter4)
+    return render_template("admin/dashboard.html", datasets=datasets, labels=labels, days=days, quarter1=quarter1, quarter2=quarter2, quarter3=quarter3, quarter4=quarter4, quarter1expenses=quarter1expenses, quarter2expenses=quarter2expenses, quarter3expenses=quarter3expenses, quarter4expenses=quarter4expenses)
