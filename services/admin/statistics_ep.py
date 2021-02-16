5e766e243823430060c3527732b3cdfb886d4206
awtoh <74389490+awtoh@users.noreply.github.com>
2021-02-16 23:27:58 +0800

    usercount daily 3
    code doesn't crash dashboard anymore

--- services/admin/statistics_ep.py
+++ services/admin/statistics_ep.py
@@ -3,7 +3,6 @@ import os
 from flask_login.utils import login_required, current_user
 from data.statistics import Settings
 from data.users import User, TableUser
-import json
 
 endpoint = Blueprint("admin_statistics", __name__)
 basedir = os.getcwd()
@@ -31,6 +30,11 @@ def page_dashboard():
     quarter2expenses = [69, 1337, 420]
     quarter3expenses = [93, 21, 37]
     quarter4expenses = [320, 203, 416]
+    final_count = [1]
+    db_users = TableUser()
+    db_users.close()
+    users = db_users.objects()
+    total_users = len(users)
     if request.method == "POST":
         print("hello world")
     return render_template(
@@ -47,16 +51,23 @@ def page_dashboard():
         quarter2expenses=quarter2expenses,
         quarter3expenses=quarter3expenses,
         quarter4expenses=quarter4expenses,
+        final_count=final_count,
+        total_users=total_users,
     )
 
-@endpoint.route('/dailyuserstotal', methods=["GET", "POST"])
+@endpoint.route("/usercount", methods=["GET", "POST"])
 def api_user_total():
-    """json => {total_users: total_users}"""
-    user_total = request.json["total_users"]
+    final_count = [1]
     db_users = TableUser()
     db_users.close()
     users = db_users.objects()
     total_users = len(users)
+    if request.method =="POST":
+        print("hello world")
+    return render_template(
+        "admin/dashboard.html",
+        final_count=final_count,
+    )
 
 
 @endpoint.route("/settings", methods=["GET", "POST"])
