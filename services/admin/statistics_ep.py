350082511d0a1c6877338a70b24df50076f8ffbf
ethan-rui <ruiheng291@gmail.com>
2021-02-17 11:04:17 +0800

    reverted dashboard changes

--- services/admin/statistics_ep.py
+++ services/admin/statistics_ep.py
@@ -3,9 +3,6 @@ import os
 from flask_login.utils import login_required, current_user
 from data.statistics import Settings
 from data.users import User, TableUser
-import json
-import datetime
-
 
 endpoint = Blueprint("admin_statistics", __name__)
 basedir = os.getcwd()
@@ -21,17 +18,7 @@ def check_perms():
 
 @endpoint.route("/", methods=["GET", "POST"])
 def page_dashboard():
-    db_users = TableUser()
-    db_users.close()
-
-    """getting the new users that were created"""
-    users = db_users.objects()
-    current_date = datetime.datetime.now().strftime("%d/%m/%Y")
-    new_users = []
-    for x in users:
-        if x.date_created.strftime("%d/%m/%Y") == current_date:
-            new_users.append(x)
-
+    usercount = [0]
     datasets = [89, 23, 63, 13, 55, 169]
     labels = ["Seafood", "Fruits", "Dairy", "Others", "Vegetables", "Meat"]
     days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
@@ -43,10 +30,16 @@ def page_dashboard():
     quarter2expenses = [69, 1337, 420]
     quarter3expenses = [93, 21, 37]
     quarter4expenses = [320, 203, 416]
+    final_count = [1]
+    db_users = TableUser()
+    db_users.close()
+    users = db_users.objects()
+    total_users = len(users)
     if request.method == "POST":
         print("hello world")
     return render_template(
         "admin/dashboard.html",
+        usercount=usercount,
         datasets=datasets,
         labels=labels,
         days=days,
@@ -58,16 +51,25 @@ def page_dashboard():
         quarter2expenses=quarter2expenses,
         quarter3expenses=quarter3expenses,
         quarter4expenses=quarter4expenses,
+        final_count=final_count,
+        total_users=total_users,
     )
 
-@endpoint.route('/dailyuserstotal', methods=["GET", "POST"])
+
+@endpoint.route("/usercount", methods=["GET", "POST"])
 def api_user_total():
-    """json => {total_users: total_users}"""
-    user_total = request.json["total_users"]
+    final_count = [1]
     db_users = TableUser()
     db_users.close()
     users = db_users.objects()
     total_users = len(users)
+    if request.method == "POST":
+        print("hello world")
+    return render_template(
+        "admin/dashboard.html",
+        final_count=final_count,
+    )
+
 
 @endpoint.route("/settings", methods=["GET", "POST"])
 def page_settings():
